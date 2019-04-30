38d39ec52d43e22c1e4d7f6e47d32a58b52e43db
Troy Walker <twalkerjr22@gmail.com>
2019-04-30 11:38:50 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/PGA-Leaderboard.git/precache-manifest.bc9c9ca282d78ae53d08bb81466989e3.js"
+  "/PGA-Leaderboard/precache-manifest.34aab2e48aa734694268c7042a44eac7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PGA-Leaderboard.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PGA-Leaderboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
